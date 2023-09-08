1c1bdf5db2ba2f746193b999d473da623fde88ad
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2023-09-08 23:45:42 +0530

    read write deleted file system with mono

---
+++ src/main/java/com/rp/sec01/Lec08MonoFromRunnable.java
@@ -0,0 +1,28 @@
+package com.rp.sec01;
+
+import com.rp.sec01.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec08MonoFromRunnable {
+    public static void main(String[] args) {
+//        Runnable runnable =()-> System.out.println("");// used to notify when the time-consuming job is completed
+
+        Mono.fromRunnable(timeConsumingProcess()).subscribe(
+                Util.onNext(),
+                Util.onError(),
+                ()->{
+                    System.out.println("process done ,sending email.....");
+                }
+        );
+    }
+
+    public static Runnable timeConsumingProcess(){
+        return ()->{
+            Util.sleepSeconds(1);
+            System.out.println("Operation Completed");
+        };
+    }
+
+
+
+}
---
+++ src/main/java/com/rp/sec01/Lec09AssignmentDemo.java
@@ -0,0 +1,16 @@
+package com.rp.sec01;
+
+import com.rp.sec01.assignment.FileService;
+import com.rp.sec01.courseutil.Util;
+
+public class Lec09AssignmentDemo {
+    public static void main(String[] args) {
+
+        FileService.write("file03.txt","hello world")
+                .subscribe(Util.onNext(),Util.onError(),Util.onComplete());
+        FileService.read("file03.txt")
+                .subscribe(Util.onNext(),Util.onError(),Util.onComplete());
+        FileService.delete("file03.txt")
+                .subscribe(Util.onNext(),Util.onError(),Util.onComplete());
+    }
+}
