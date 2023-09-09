ddd6596f23901914a0869bb70df086b449c96c1d
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2023-09-09 20:45:40 +0530

    flux from range

---
+++ src/main/java/com/rp/sec02/Lec05FluxFromRange.java
@@ -0,0 +1,16 @@
+package com.rp.sec02;
+
+import com.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec05FluxFromRange {
+
+    public static void main(String[] args) {
+        Flux.range(3,10)
+//                .map(i->i*2)
+                .map(i->Util.faker().funnyName().name())
+                .subscribe(
+                        Util.onNext()
+                );
+    }
+}
