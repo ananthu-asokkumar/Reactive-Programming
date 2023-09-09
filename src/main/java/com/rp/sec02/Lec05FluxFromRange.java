46fa642be49fbad60c6cd5b710e7635659507cb6
ananthu-asokkumar <ananthuasokkumar@gmail.com>
2023-09-09 21:13:28 +0530

    custom subscriber

--- src/main/java/com/rp/sec02/Lec05FluxFromRange.java
+++ src/main/java/com/rp/sec02/Lec05FluxFromRange.java
@@ -8,7 +8,9 @@ public class Lec05FluxFromRange {
     public static void main(String[] args) {
         Flux.range(3,10)
 //                .map(i->i*2)
+                .log()
                 .map(i->Util.faker().funnyName().name())
+                .log()
                 .subscribe(
                         Util.onNext()
                 );
